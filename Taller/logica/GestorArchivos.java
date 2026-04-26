b93f77e0a89cd94f295ccaceeaf6c077da67bdfb
rarojas25 <rocio.rojas02@alumnos.ucn.cl>
2026-04-26 02:02:00 -0400

    add Gestor Archivos con lectura y escritura

---
+++ Taller/logica/GestorArchivos.java
@@ -0,0 +1,10 @@
+package logica;
+
+public class GestorArchivos {
+	private static final String RUTA_RESGISTROS = "Registros.txt";
+	private static final String RUTA_HABITATS = "Habitats.txt";
+	private static final String RUTA_POQUEDEX = "Pokedex.txt";
+	private static final String RUTA_GIMNASIOS = "Gimnasios.txt";
+	private static final String RUTA_ALTO_MANDO = "Alto Mando.txt";
+	
+}
